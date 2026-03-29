a1d5dc21eb86d059235c34148ae3e49d89132b60
Maruan Bakri Ottoni <maruanbakriottoni@gmail.com>
2026-03-29 18:48:59 -0300

    Add: plus-one - submission-6

---
+++ Data Structures & Algorithms/plus-one/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 0 
+        digits = digits[::-1]
+        for i in range(len(digits)):
+            if i==0:
+                soma = (digits[i]+1+carry) % 10
+                carry = (digits[i]+1+carry) // 10 
+                digits[i] = soma
+            else:
+                soma = (digits[i]+carry) % 10
+                carry = (digits[i]+carry) // 10
+                digits[i] = soma
+                
+        if carry > 0:
+            digits = digits+[1]
+        return digits[::-1]

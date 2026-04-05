24af4128b7a85eba7156adf80a13044f541d61a3
Maruan Bakri Ottoni <maruanbakriottoni@gmail.com>
2026-04-05 17:58:44 -0300

    Add: longest-palindromic-substring - submission-17

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-17.py
@@ -0,0 +1,22 @@
+from functools import cache
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        max_len = []
+
+        @cache
+        def dfs(i, j):
+            if i >= 0 and j < n:
+                if s[i] == s[j]:
+                    max_len.append(s[i:j+1])  # append before recursing
+                    dfs(i-1, j+1)
+                else:
+                    return
+            else:
+                return
+
+        for mid in range(n):
+            dfs(mid, mid)
+            dfs(mid, mid+1)
+
+        return max(max_len, key=len)

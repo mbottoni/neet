7e9d9a96c9571a7e942b3f9004f6985cfb86e554
Maruan Bakri Ottoni <maruanbakriottoni@gmail.com>
2026-04-08 19:22:19 -0300

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = (n + 1) * n // 2
+        print(expected_sum)
+
+        total_sum = sum(nums)
+
+        return expected_sum - total_sum
+        

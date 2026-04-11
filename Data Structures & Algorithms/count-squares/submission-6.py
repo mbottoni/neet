437b343e07450a5482c35e962e36da9c2c76bf55
Maruan Bakri Ottoni <maruanbakriottoni@gmail.com>
2026-04-10 22:03:29 -0300

    Add: count-squares - submission-6

---
+++ Data Structures & Algorithms/count-squares/submission-6.py
@@ -0,0 +1,22 @@
+class CountSquares:
+    def __init__(self):
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.points.append(point)
+
+    def count(self, point: List[int]) -> int:
+        from collections import Counter
+        point_count = Counter(map(tuple, self.points))
+        
+        align_x = [p for p in self.points if p[0] == point[0]]
+        align_y = [p for p in self.points if p[1] == point[1]]
+    
+        count = 0
+        for px in align_x:
+            for py in align_y:
+                if abs(px[1] - point[1]) != abs(py[0] - point[0]) or px[1] == point[1]:
+                    continue
+                count += point_count[(py[0], px[1])]
+    
+        return count

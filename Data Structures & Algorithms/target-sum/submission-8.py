b4fce0839d17799b2b7025d1961622f25c87efe8
Maruan Bakri Ottoni <maruanbakriottoni@gmail.com>
2026-04-12 20:46:59 -0300

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.py
@@ -0,0 +1,11 @@
+from functools import cache
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        @cache
+        def run(index, soma):
+            if index == len(nums):
+                return 1 if soma == target else 0
+            return run(index+1, soma-nums[index])+run(index+1, soma+nums[index])
+        return run(0, 0)
+
+
